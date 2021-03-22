cd1f793527c4579b6fa8f6a8b51d2f33a3cf39ad
Tiago Missão <t.missao@gmail.com>
2021-03-22 13:11:28 -0300

    Update output.tf

--- kafka-setup/terraform/modules/vpc/output.tf
+++ kafka-setup/terraform/modules/vpc/output.tf
@@ -3,11 +3,6 @@ output "vpc_id" {
   value       = "${aws_vpc.vpc.id}"
 }
 
-output "ssm_vpc_id" {
-  description = "SSM Key for VPC ID"
-  value = "${module.SsmVpcId.parameter_name}"
-}
-
 output "vpc_cidr_block" {
   description = "The Cidr Block of the VPC"
   value       = "${aws_vpc.vpc.cidr_block}"
@@ -16,11 +11,6 @@ output "vpc_cidr_block" {
 output "public_subnets" {
   description = "The Ids of public subnets"
   value       = "${aws_subnet.public.*.id}"
-}
-
-output "ssm_public_subnets_ids" {
-  description = "SSM Key for VPC`s public subnets"
-  value = "${module.SsmPublicSubnetIds.parameter_name}"
 }
 
 output "public_subnets_cidr_block" {
@@ -35,11 +25,6 @@ output "public_routing_table_id" {
 output "private_subnets" {
   description = "The Ids of private subnets"
   value       = "${aws_subnet.private.*.id}"
-}
-
-output "ssm_private_subnets_ids" {
-  description = "SSM Key for VPC`s private subnets"
-  value = "${module.SsmPrivateSubnetIds.parameter_name}"
 }
 
 output "private_subnets_cidr_block" {
